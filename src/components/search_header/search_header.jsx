af5d18f5f3857d1e7273bc787ccce1cd4d4cbc7d
HyungjunJeon <maru105kr@gmail.com>
2020-12-11 20:01:07 +0900

    Back to the list when searching

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -6,6 +6,7 @@ const SearchHeader = ({ onSearch }) => {
   const handleSearch = () => {
     const value = inputRef.current.value;
     onSearch(value);
+    inputRef.current.value = "";
   };
   const onClick = () => {
     handleSearch();
